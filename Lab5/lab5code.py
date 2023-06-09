49a39e1248b68822dc7d9b2a87df33c854d3d05a
Pryshlitsa Danylo <127943233+danyadp@users.noreply.github.com>
2023-06-09 18:55:00 +0300

    lab5code.py

--- Lab5/lab5code.py
+++ Lab5/lab5code.py
@@ -1,9 +1,27 @@
-number_dict = { "0": "zero", "1": "one", "2": "two", "3": "three", "4": "four", "5": "five", "6": "six", "7": "seven", "8": "eight", "9": "nine" }
+from typing import Dict
+
+number_dict: Dict[str, str] = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+}
 
 print("Привіт! Я перетворюю номер у словесне представлення.")
 
 number = input("Будь ласка, введіть номер: ")
 
-if not number.isdigit(): print("Ви ввели неправильний номер.") else: # Перетворюємо номер на словесне представлення word_number = "" for digit in number: word_number += number_dict[digit] + " "
+if not number.isdigit():
+    print("Ви ввели неправильний номер.")
+else:
+    word_number = ""
+    for digit in number:
+        word_number += number_dict[digit] + " "
 
-print("Ваш номер у словесному представленні: ", word_number)
+    print("Ваш номер у словесному представленні:", word_number)
